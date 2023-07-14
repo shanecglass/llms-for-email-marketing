502f4087877032d7feca5b897fe45a00374e5117
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2023-07-14 13:23:02 -0400

    Initial commit for testing

---
+++ init
@@ -0,0 +1,2 @@
+gcloud services enable cloudbuild.googleapis.com
+gcloud services enable run.googleapis.com
---
+++ modules.py
@@ -0,0 +1,53 @@
+# This file will get executed when the function is executed
+import json
+import vertexai
+
+from concurrent import futures
+from google.cloud import pubsub_v1
+# from google.cloud import secretmanager
+from typing import Callable
+from vertexai.preview.language_models import TextGenerationModel, TextEmbeddingModel
+
+project_id = "scg-test-and-dev"
+location = "us-central1"
+pubsub_topic_id = "email_marketing_llm"
+
+vertexai.init(project=project_id, location=location)
+
+def get_text_embeddings(input):
+  model = TextEmbeddingModel.from_pretrained("textembedding-gecko@001")
+  try:
+    embeddings = model.get_embeddings([input])
+    output = [embedding.values for embedding in embeddings]
+    return output
+  except Exception:
+    return [None for _ in range(len(input))]
+
+def get_response(input_prompt):
+  model = TextGenerationModel.from_pretrained("text-bison@001")
+  parameters = {
+        "temperature": 0.9,  # Temperature controls the degree of randomness in token selection.
+        "max_output_tokens": 512,  # Token limit determines the maximum amount of text output.
+        "top_p": 0.8,  # Tokens are selected from most probable to least until the sum of their probabilities equals the top_p value.
+        "top_k": 40,  # A top_k of 1 means the selected token is the most probable among all tokens.
+  }
+
+  output = model.predict(
+    prompt=input_prompt,
+    **parameters
+  )
+  print(output.text)
+  candidates = json.dumps(output._prediction_response)
+  return candidates
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(project_id, pubsub_topic_id)
+
+def publish_pubsub(text, embedding):
+  #"""Text embedding with a Large Language Model."""
+      data_string = json.dumps(f"{text},{embedding}")
+    # Data must be a bytestring
+      data = data_string.encode("utf-8")
+    # When you publish a message, the client returns a future.
+      future = publisher.publish(topic_path, data)
+      print(future.result())
