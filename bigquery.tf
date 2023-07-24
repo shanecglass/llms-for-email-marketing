5be531e66e40818f9c128f4c9a7b1ae7bbdf4e51
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2023-07-24 16:33:16 -0400

    bug testing

--- bigquery.tf
+++ bigquery.tf
@@ -72,14 +72,15 @@ resource "google_bigquery_job" "load_samples" {
     "my_job" ="load"
   }
 
-  dynamic "load" {
+  dynamic load {
     for_each = var.resource_purpose
-    # content {
-    source_uris = "${var.sample_data_bucket}${each.key}.parquet"
+    iterator = purpose
+    content {
+    source_uris = "${var.sample_data_bucket}${purpose.key}.parquet"
     destination_table {
       project_id = module.project-services.project_id
       dataset_id = google_bigquery_dataset.dest_dataset.dataset_id
-      table_id   = each.key
+      table_id   = purpose.key
     }
     write_disposition     = "WRITE_EMPTY"
     source_format         = "PARQUET"
