e89d996c3a13568ed2d8c823bc7739c2c6a12551
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2023-07-25 15:54:22 -0400

    env-vars to deploy.sh

--- app/deploy.sh
+++ app/deploy.sh
@@ -4,10 +4,11 @@ IMG_TAG="gcr.io/$PROJ/email-marketing-llm"
 SERVICE_ACCOUNT="demo-app@$PROJ.iam.gserviceaccount.com"
 
 gcloud run deploy "email-marketing-llm-app"  \
-    --project "$PROJ"     \
-    --image "$IMG_TAG"    \
-    --platform "managed"  \
-    --port 5000           \
-    --region "$REGION"    \
+    --project "$PROJ"                \
+    --image "$IMG_TAG"               \
+    --update-env-vars PROJ=$PROJ     \
+    --platform "managed"             \
+    --port 5000                      \
+    --region "$REGION"               \
     --service-account "$SERVICE_ACCOUNT" \
     --allow-unauthenticated
